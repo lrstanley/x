9b3fe1e04974a5737fed5a8ecea87f74f89d30d5
Liam Stanley <liam@liam.sh>
2025-11-22 16:14:53 -0700

    add examples
    Signed-off-by: Liam Stanley <liam@liam.sh>

---
+++ charm/layout/examples/go.mod
@@ -0,0 +1,32 @@
+module github.com/lrstanley/x/charm/layout/examples
+
+go 1.25.4
+
+replace github.com/lrstanley/x/charm/layout => ../
+
+require (
+	charm.land/bubbletea/v2 v2.0.0-rc.2.0.20251120234600-e78528df7958
+	charm.land/lipgloss/v2 v2.0.0-beta.3.0.20251121225325-f6fbdf23b0ff
+	github.com/charmbracelet/x/exp/charmtone v0.0.0-20251118172736-77d017256798
+	github.com/lrstanley/x/charm/layout v0.0.0-20251122231207-4180e84d035f
+)
+
+require (
+	github.com/charmbracelet/colorprofile v0.3.3 // indirect
+	github.com/charmbracelet/ultraviolet v0.0.0-20251120225753-26363bddd922 // indirect
+	github.com/charmbracelet/x/ansi v0.11.1 // indirect
+	github.com/charmbracelet/x/exp/color v0.0.0-20250915100343-2c2e5896ae6e // indirect
+	github.com/charmbracelet/x/term v0.2.2 // indirect
+	github.com/charmbracelet/x/termios v0.1.1 // indirect
+	github.com/charmbracelet/x/windows v0.2.2 // indirect
+	github.com/clipperhouse/displaywidth v0.6.0 // indirect
+	github.com/clipperhouse/stringish v0.1.1 // indirect
+	github.com/clipperhouse/uax29/v2 v2.3.0 // indirect
+	github.com/lucasb-eyer/go-colorful v1.3.0 // indirect
+	github.com/mattn/go-runewidth v0.0.19 // indirect
+	github.com/muesli/cancelreader v0.2.2 // indirect
+	github.com/rivo/uniseg v0.4.7 // indirect
+	github.com/xo/terminfo v0.0.0-20220910002029-abceb7e1c41e // indirect
+	golang.org/x/sync v0.18.0 // indirect
+	golang.org/x/sys v0.38.0 // indirect
+)
---
+++ charm/layout/examples/kitchensink/main.go
@@ -0,0 +1,118 @@
+package main
+
+import (
+	"fmt"
+	"image/color"
+	"log/slog"
+	"os"
+
+	tea "charm.land/bubbletea/v2"
+	"charm.land/lipgloss/v2"
+	"github.com/charmbracelet/x/exp/charmtone"
+	"github.com/lrstanley/x/charm/layout"
+)
+
+type model struct {
+	width  int
+	height int
+}
+
+func (m model) Init() tea.Cmd {
+	return nil
+}
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case layout.LayerMouseMsg:
+		slog.Debug("Layer hit", "layer", msg.LayerID, "mouse", msg.Mouse, "type", fmt.Sprintf("%T", msg.Mouse)) //nolint:sloglint
+		if mouse, ok := msg.Mouse.(tea.MouseClickMsg); ok {
+			return m, tea.Printf("Layer hit at %d, %d", mouse.X, mouse.Y)
+		}
+	case tea.WindowSizeMsg:
+		m.width = msg.Width
+		m.height = msg.Height
+		return m, nil
+	case tea.KeyPressMsg:
+		switch msg.String() {
+		case "q", "ctrl+c", "esc":
+			return m, tea.Quit
+		}
+	}
+	return m, nil
+}
+
+func (m model) View() tea.View {
+	var view tea.View
+
+	view.AltScreen = true
+	view.MouseMode = tea.MouseModeCellMotion
+
+	footer := lipgloss.NewStyle().
+		Foreground(charmtone.Oyster).
+		AlignVertical(lipgloss.Bottom).
+		Render("Press any key to swap the cards, or q to quit.")
+
+	layout.RenderView(
+		&view,
+		m.width,
+		m.height,
+		layout.Stack(
+			layout.Vertical(
+				layout.Horizontal(
+					layout.Stack(
+						newCard("card_1", "Hello", charmtone.Charple),
+						newCard("card_2", "left horizontal", charmtone.Charple).X(4).Y(2),
+					),
+					layout.Space(),
+					layout.Stack(
+						newCard("card_3", "Hello", charmtone.Charple),
+						newCard("card_4", "right horizontal", charmtone.Charple).X(4).Y(2),
+					),
+				),
+				layout.Frame(
+					lipgloss.NewStyle().Border(lipgloss.RoundedBorder()).BorderForeground(charmtone.Charple),
+					"framed text",
+				),
+				layout.Space(),
+				footer,
+			),
+			layout.Center(
+				newCard("card_5", "Centered Card", charmtone.Charple),
+			),
+		),
+	)
+
+	return view
+}
+
+func newCard(id, str string, border color.Color) *lipgloss.Layer {
+	return lipgloss.NewLayer(
+		id,
+		lipgloss.NewStyle().
+			Width(20).
+			Height(10).
+			Border(lipgloss.RoundedBorder()).
+			BorderForeground(border).
+			Align(lipgloss.Center, lipgloss.Center).
+			Render(str),
+	)
+}
+
+func main() {
+	// log slog to file, using json.
+	f, err := os.OpenFile("debug.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0o600)
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+
+	slog.SetDefault(slog.New(slog.NewJSONHandler(f, &slog.HandlerOptions{
+		Level: slog.LevelDebug,
+	})))
+
+	_, err = tea.NewProgram(model{}).Run()
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "Urgh:", err)
+		os.Exit(1)
+	}
+}
