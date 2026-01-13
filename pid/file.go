9400623c9cc0e10a3a786dd5894747fd347000fc
Liam Stanley <liam@liam.sh>
2026-01-12 20:51:49 -0700

    feat: addl fallbacks for pid pkg, logging, etc
    Signed-off-by: Liam Stanley <liam@liam.sh>

--- pid/file.go
+++ pid/file.go
@@ -7,6 +7,7 @@ package pid
 import (
 	"errors"
 	"fmt"
+	"log/slog"
 	"os"
 	"os/signal"
 	"path/filepath"
@@ -28,6 +29,7 @@ type File struct {
 
 	mu          sync.RWMutex
 	appID       string
+	logger      *slog.Logger
 	dir         string
 	signal      syscall.Signal
 	onSecondary func([]string)
@@ -35,9 +37,18 @@ type File struct {
 
 // New creates a pidfile instance based on the provided application ID.
 func New(appID string) *File {
+	dir := os.TempDir()
+	if dir == "" {
+		cwd, err := os.Getwd()
+		if err != nil {
+			panic(err)
+		}
+		dir = cwd
+	}
 	return &File{
 		appID:       appID,
-		dir:         os.TempDir(),
+		logger:      slog.New(slog.DiscardHandler),
+		dir:         dir,
 		signal:      syscall.SIGTERM,
 		onSecondary: nil,
 	}
@@ -49,6 +60,20 @@ func (pf *File) WithAppID(appID string) *File {
 	pf.appID = appID
 	pf.mu.Unlock()
 	return pf
+}
+
+// WithLogger sets the logger for the pidfile.
+func (pf *File) WithLogger(logger *slog.Logger) *File {
+	pf.mu.Lock()
+	pf.logger = logger
+	pf.mu.Unlock()
+	return pf
+}
+
+func (pf *File) log() *slog.Logger {
+	pf.mu.RLock()
+	defer pf.mu.RUnlock()
+	return pf.logger
 }
 
 // WithDir sets the directory for the pidfile. Defaults to the system temp directory.
@@ -121,12 +146,15 @@ func (pf *File) Create() error {
 			signal.Notify(c, sig)
 			for {
 				<-c
+				pf.log().Debug("secondary process signal received", "signal", sig)
 				_, err := os.Stat(pf.path() + ".args")
 				if errors.Is(err, os.ErrNotExist) {
-					os.Exit(1)
+					pf.log().Debug("secondary process args file not found", "path", pf.path()+".args")
+					continue
 				}
 				args, _ := os.ReadFile(pf.path() + ".args")
 				_ = os.Remove(pf.path() + ".args")
+				pf.log().Debug("secondary process args file found, invoking hook", "path", pf.path()+".args", "args", string(args))
 				hook(strings.Split(string(args), "\b"))
 			}
 		}()
@@ -136,11 +164,14 @@ func (pf *File) Create() error {
 	pid := os.Getpid()
 	f, err := os.OpenFile(pf.path(), os.O_WRONLY|os.O_CREATE|os.O_EXCL, 0o640) //nolint:gosec
 	if err == nil {
+		pf.log().Debug("pidfile created", "path", pf.path(), "pid", pid)
 		_, _ = f.WriteString(strconv.Itoa(pid))
 		_ = f.Sync()
 		_ = f.Close()
 		return nil
 	}
+	_ = f.Close()
+
 	if !os.IsExist(err) {
 		return err
 	}
@@ -157,49 +188,48 @@ func (pf *File) Create() error {
 	}
 
 	process := lookupProcess(pid)
-	if process != nil {
-		pf.firstPID = pid
-		if hook != nil {
-			var file *os.File
-			for range MaxSignalRetries {
-				file, err = os.OpenFile(pf.path()+".args.lock", os.O_WRONLY|os.O_CREATE|os.O_EXCL, 0o640) //nolint:gosec
-				if err == nil {
-					defer func() { //nolint:gocritic
-						_ = file.Close()
-						_ = os.Remove(pf.path() + ".args.lock")
-					}()
-					break
-				}
-				time.Sleep(SignalRetryDelay)
-			}
 
-			err = os.WriteFile(pf.path()+".args", []byte(strings.Join(os.Args, "\b")), 0o640) //nolint:gosec
-			if err != nil {
-				return err
-			}
-
-			time.Sleep(SignalRetryDelay)
-
-			err = process.Signal(sig)
-			if err != nil {
-				return err
-			}
-
-			for range MaxSignalRetries {
-				_, err = os.Stat(pf.path() + ".args")
-				if os.IsNotExist(err) {
-					break
-				}
-			}
+	if process == nil {
+		// Process not found, but file exists, delete and recreate it.
+		pf.log().Debug("process not found, deleting and recreating pidfile", "path", pf.path())
+		err = os.Remove(pf.path())
+		if err != nil {
+			return err
 		}
-		return nil
+		return os.WriteFile(pf.path(), []byte(strconv.Itoa(os.Getpid())), 0o640) //nolint:gosec
 	}
 
-	err = os.Remove(pf.path())
-	if err != nil {
-		return err
+	pf.firstPID = pid
+	if hook != nil {
+		var file *os.File
+		for range MaxSignalRetries {
+			pf.log().Debug("attempting to open args lock file", "path", pf.path()+".args.lock")
+			file, err = os.OpenFile(pf.path()+".args.lock", os.O_WRONLY|os.O_CREATE|os.O_EXCL, 0o640) //nolint:gosec
+			if err == nil {
+				defer func() { //nolint:gocritic
+					_ = file.Close()
+					_ = os.Remove(pf.path() + ".args.lock")
+				}()
+				break
+			}
+			time.Sleep(SignalRetryDelay)
+		}
+
+		pf.log().Debug("writing args to file", "path", pf.path()+".args", "args", strings.Join(os.Args, "\b"))
+		err = os.WriteFile(pf.path()+".args", []byte(strings.Join(os.Args, "\b")), 0o640) //nolint:gosec
+		if err != nil {
+			return err
+		}
+
+		time.Sleep(SignalRetryDelay)
+
+		pf.log().Debug("signaling primary process", "signal", sig)
+		err = process.Signal(sig)
+		if err != nil {
+			return err
+		}
 	}
-	return os.WriteFile(pf.path(), []byte(strconv.Itoa(os.Getpid())), 0o640) //nolint:gosec
+	return nil
 }
 
 // Remove removes the pidfile.
@@ -210,9 +240,11 @@ func (pf *File) Remove() error {
 	}
 
 	if string(pid) != strconv.Itoa(os.Getpid()) {
+		pf.log().Debug("pidfile does not match current process, skipping removal", "path", pf.path(), "pid", string(pid), "current_pid", os.Getpid())
 		return nil
 	}
 
+	pf.log().Debug("removing pidfile", "path", pf.path())
 	err = os.Remove(pf.path())
 	if err != nil {
 		return err
@@ -223,7 +255,9 @@ func (pf *File) Remove() error {
 	pf.mu.RUnlock()
 
 	if hook != nil {
+		pf.log().Debug("removing args lock and args files", "path", pf.path()+".args.lock", "path", pf.path()+".args")
 		_ = os.Remove(pf.path() + ".args.lock")
+		pf.log().Debug("removed args lock file", "path", pf.path()+".args.lock")
 		_ = os.Remove(pf.path() + ".args")
 	}
 	return nil
