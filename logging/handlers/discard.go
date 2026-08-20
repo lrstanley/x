c29a6d098dc7931963a6cdcb4b5e0ac2959f34d5
Liam Stanley <liam@liam.sh>
2026-08-19 22:58:12 -0600

    refactor(logging): add go:fix inline for discard and fanout handlers
    Signed-off-by: Liam Stanley <liam@liam.sh>

--- logging/handlers/discard.go
+++ logging/handlers/discard.go
@@ -5,36 +5,13 @@
 package handlers
 
 import (
-	"context"
 	"log/slog"
 )
-
-// discard discards all log records.
-type discard struct{}
 
 // NewDiscard creates a new discard handler.
 //
 // Deprecated: Use [log/slog.DiscardHandler] instead.
+//go:fix inline
 func NewDiscard() slog.Handler {
-	return &discard{}
-}
-
-// Enabled implements the [log/slog.Handler] interface.
-func (h *discard) Enabled(_ context.Context, _ slog.Level) bool {
-	return false
-}
-
-// Handle implements the [log/slog.Handler] interface.
-func (h *discard) Handle(_ context.Context, _ slog.Record) error {
-	return nil
-}
-
-// WithAttrs implements the [log/slog.Handler] interface.
-func (h *discard) WithAttrs(_ []slog.Attr) slog.Handler {
-	return h
-}
-
-// WithGroup implements the [log/slog.Handler] interface.
-func (h *discard) WithGroup(_ string) slog.Handler {
-	return h
+	return slog.DiscardHandler
 }
--- logging/handlers/fanout.go
+++ logging/handlers/fanout.go
@@ -5,92 +5,14 @@
 package handlers
 
 import (
-	"context"
-	"errors"
-	"fmt"
 	"log/slog"
 )
-
-var _ slog.Handler = (*fanout)(nil) // Ensure we implement the [log/slog.Handler] interface.
-
-// fanout distributes log records to multiple [log/slog.Handler] instances.
-type fanout struct {
-	handlers []slog.Handler
-}
 
 // NewFanout creates a new fanout handler that distributes records to multiple
 // [log/slog.Handler] instances.
 //
 // Deprecated: use [log/slog.NewMultiHandler] instead.
+//go:fix inline
 func NewFanout(handlers ...slog.Handler) slog.Handler {
-	return &fanout{
-		handlers: handlers,
-	}
-}
-
-// Enabled checks if any of the underlying handlers are enabled for the given
-// level. The handler is considered enabled if at least one of its child handlers
-// is enabled for the specified level. If at least one handler can process the log,
-// the fanout handler will attempt to distribute it.
-func (h *fanout) Enabled(ctx context.Context, l slog.Level) bool {
-	for i := range h.handlers {
-		if h.handlers[i].Enabled(ctx, l) {
-			return true
-		}
-	}
-	return false
-}
-
-// Handle distributes a log record to all enabled handlers.
-func (h *fanout) Handle(ctx context.Context, r slog.Record) error {
-	var errs []error
-	for i := range h.handlers {
-		if h.handlers[i].Enabled(ctx, r.Level) {
-			err := try(func() error {
-				return h.handlers[i].Handle(ctx, r.Clone())
-			})
-			if err != nil {
-				errs = append(errs, err)
-			}
-		}
-	}
-	return errors.Join(errs...)
-}
-
-// WithAttrs creates a new handler with additional attributes added to all child
-// handlers.
-func (h *fanout) WithAttrs(attrs []slog.Attr) slog.Handler {
-	handlers := make([]slog.Handler, len(h.handlers))
-	for i := range h.handlers {
-		handlers[i] = h.handlers[i].WithAttrs(attrs)
-	}
-	return NewFanout(handlers...)
-}
-
-// WithGroup creates a new handler with a group name applied to all child
-// handlers.
-func (h *fanout) WithGroup(name string) slog.Handler {
-	// https://cs.opensource.google/go/go/+/master:src/log/slog/handler.go;drc=3fd729b2a14a7efcf08465cbea60a74da5457f06;l=90
-	if name == "" {
-		return h
-	}
-	handlers := make([]slog.Handler, len(h.handlers))
-	for i := range h.handlers {
-		handlers[i] = h.handlers[i].WithGroup(name)
-	}
-	return NewFanout(handlers...)
-}
-
-func try(callback func() error) (err error) {
-	defer func() {
-		if r := recover(); r != nil {
-			if perr, ok := r.(error); ok {
-				err = perr
-			} else {
-				err = fmt.Errorf("panic: %+v", r)
-			}
-		}
-	}()
-	err = callback()
-	return err
+	return slog.NewMultiHandler(handlers...)
 }
